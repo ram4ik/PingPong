a13dba56b91d063fb6152d0675d9d573389c46eb
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-12-24 18:05:17 +0200

    Initial Commit

---
+++ PingPong WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PingPong WatchKit Extension
+//
+//  Created by ramil on 24.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PingPong WatchKit Extension/PingPongApp.swift
@@ -0,0 +1,19 @@
+//
+//  PingPongApp.swift
+//  PingPong WatchKit Extension
+//
+//  Created by ramil on 24.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct PingPongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
